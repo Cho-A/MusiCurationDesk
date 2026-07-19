0f3ffd07d3118e3f2bbca1ad05624587628ea7fa
Cho-A <takanoryo365947@gmail.com>
2026-07-19 20:55:26 +0900

    fix(backend): Add 127.0.0.1 to CORS origins

--- app/main.py
+++ app/main.py
@@ -56,6 +56,8 @@ app.include_router(external_search.router)
 origins = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
