5f81de3beca13824f82549f248c7ac2e51451052
Cho-A <takanoryo365947@gmail.com>
2026-08-12 15:26:17 +0900

    chore:不要なファイルの削除

--- test_api.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from backend.main import app
-
-client = TestClient(app, raise_server_exceptions=True)
-try:
-    response = client.get("/songs/1")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
